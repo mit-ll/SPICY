ead50330b93fe50796ed90f34db8d5b2afd61ef9
Alice R. Lee <alee122@gmail.com>
2018-09-11 09:28:26 -0400

    This commit is an attempt to retrace a step that wasn't properly documented. (#9)
    The good:
    * This commit splits the universe and individual steps.
    * The state of the channel vectors is appropriately updated.
    
    The bad:
    * No restrictions based on channel properties, obviously will fail proofs.
    * No polymorphism.
    
    So why include it? I think it clarifies the transition to polymorphism and correctly splitting out different types of channel in order to force the user to justify read/write access.

--- IdealWorld.v
+++ IdealWorld.v
@@ -38,12 +38,13 @@ Record channel_data := construct_channel
     messages_sent : list (user_id * message) ;
     user_pointers : fmap user_id nat }.
 
+Definition channels := fmap channel_id channel_data.
+
 Definition send_message m ch_d :=
   {| properties := ch_d.(properties) ;
      type := ch_d.(type) ;
      messages_sent := m :: ch_d.(messages_sent) ;
      user_pointers := ch_d.(user_pointers) |}.
-
 
 Fixpoint recv_message' ms (u : user_id) i : option message :=
   match ms with
@@ -89,49 +90,33 @@ Record universe := construct_universe
     A trace of all messages sent on all channels
 *)
   { channel_vector : fmap channel_id channel_data ;
-    users : fmap user_id cmd ;
-    trace : list (channel_id * (user_id * message)) }.
+    users : fmap user_id cmd}.
 
-Inductive step : universe -> universe -> Prop :=
-| StepCreateChannel : forall U ch_id channel_vector' p u users',
-    ~(ch_id \in dom U.(channel_vector)) ->
-    channel_vector' = U.(channel_vector) $+ (ch_id, construct_channel p Symmetric [] $0) ->
-    U.(users) $? u = Some (CreateChannel p) ->
-    users' = U.(users) $+ (u, Return (ChannelId ch_id)) ->
-    step U (construct_universe channel_vector' users' U.(trace))
-| StepSend : forall U u ch_id m users' channel_vector' ch_d,
-    U.(users) $? u = Some (Send (ChannelId ch_id) m) ->
-    users' = U.(users) $+ (u, Return (ProtocolMsg "")) ->
-    U.(channel_vector) $? ch_id = Some (ch_d) ->
-    channel_vector' = U.(channel_vector) $+ (ch_id, send_message (u, m) ch_d) ->
-    step U (construct_universe channel_vector' users' ((ch_id, (u, m))::U.(trace)))
-| StepRecv : forall U u ch_id users' m channel_vector' ch_d,
-    U.(users) $? u = Some (Recv (ChannelId ch_id)) ->
-    users' = U.(users) $+ (u, Return m) ->
-    U.(channel_vector) $? ch_id = Some ch_d ->
-    Some m = recv_message ch_d u ->
-    channel_vector' = U.(channel_vector) $+ (ch_id, inc_pointer ch_d u) ->
-    step U (construct_universe  channel_vector' users' U.(trace))
-| StepBindRecur : forall U u c1 c2,
-    U.(users) $? u = Some (Bind c1 c2) ->
-    (* TODO not sure how to do this: 
-       only change u to protocol c1?
-       put (u, c1) in a universe with no other users? 
-       should I parallelize all users at the start? *)
-    step U (construct_universe U.(channel_vector) U.(users) U.(trace))
-| StepBindProceed : forall U u r c2 users',
-    U.(users) $? u = Some (Bind (Return r) c2) ->
-    users' = U.(users) $+ (u, (c2 r)) ->
-    step U (construct_universe U.(channel_vector) users' U.(trace)).
+Inductive step_user : user_id * channels * cmd -> user_id * channels * cmd -> Prop :=
+| StepBindRecur : forall u (c1 c1' : cmd) (c2 : message -> cmd) cv cv',
+    step_user (u, cv, c1) (u, cv', c1') ->
+    step_user (u, cv, (Bind c1 c2)) (u, cv', (Bind c1' c2))
+| StepBindProceed : forall u v (c2 : message -> cmd ) cv,
+    step_user (u, cv, (Bind (Return v) c2)) (u, cv, c2 v)
+| StepCreateChannel : forall ch_id p cv u,
+    ~(ch_id \in dom cv) ->
+    step_user (u, cv, (CreateChannel p))
+              (u, (cv $+ (ch_id, construct_channel p Symmetric [] $0)), (Return (ChannelId ch_id)))
+| StepSend : forall ch_id m cv ch_d u,
+    cv $? ch_id = Some ch_d ->
+    step_user (u, cv, Send (ProtocolMsg m) (ChannelId ch_id))
+              (u, cv $+ (ch_id, send_message (u, ProtocolMsg m) ch_d), Return (ProtocolMsg ""))
+| StepRecv : forall ch_id m cv ch_d u,
+    cv $? ch_id = Some ch_d ->
+    recv_message ch_d u = Some m -> 
+    step_user (u, cv, Recv (ChannelId ch_id)) (u, (cv $+ (ch_id, (inc_pointer ch_d u))), Return m).
 
+Inductive step_universe : universe -> universe -> Prop :=
+| StepUser : forall U u u_d cs' u_d',
+    U.(users) $? u = Some u_d ->
+    step_user (u, U.(channel_vector), u_d) (u, cs', u_d') ->
+    step_universe U (construct_universe cs' (U.(users) $+ (u, u_d'))).
 
-(* Not fully implemented.
- * The users from universe
- * Questions:
- * 1. How does a user reference a channel_id when sending a message? Send takes a channel_id and a message, 
-       but the channel_id is stored in memory as fmap var message. So, need to do some look-up within the 
-       cmd? i.e., accessing memory from protocol.
- *)
 Example ping :=
   $0 $+ ("0", x <- (Send (ProtocolMsg "Hello") (ChannelId 0)) ; (Recv (ChannelId 0)))
      $+ ("1", x <- (Recv (ChannelId 0)) ; (Send x (ChannelId 0))).
@@ -143,8 +128,7 @@ Example ping_universe :=
                                   type := Default "1" "2" ;
                                   messages_sent := [] ;
                                   user_pointers := $0 |}) ;
-   users := ping ;
-   trace := [] |}.
+   users := ping |}.
 Check ping_universe.
 
 
@@ -184,7 +168,7 @@ end.
 Fixpoint add_CIA_channels (plist : list (user_id * user_id))
                           (ch_id : channel_id)
                           (umap : fmap user_id cmd) : universe :=
-let empty_universe := {| channel_vector := $0 ; users := umap ; trace := [] |} in
+let empty_universe := {| channel_vector := $0 ; users := umap |} in
 let empty_cmd := Return (ProtocolMsg "Placeholder") in
 match plist with
 | [] => empty_universe
@@ -205,8 +189,7 @@ match plist with
                                                                                                    messages_sent := [];
                                                                                                    user_pointers := $0 |})) ;
                                        users := (next_iter.(users) $+ (id1, id1_data)
-                                                                   $+ (id2, id2_data)) ;
-                                       trace := [] |}
+                                                                   $+ (id2, id2_data)) |}
               end
 end.
 
@@ -221,8 +204,7 @@ match user_id_list with
                                                                                type := Broadcast uid ;
                                                                                messages_sent := [] ;
                                                                                user_pointers := $0|}) ; 
-                            users := U.(users) ;
-                            trace := U.(trace) |}
+                            users := U.(users) |}
                in
                add_Broadcast_channels tail
                                       (ch_id + 1)
@@ -235,7 +217,7 @@ end.
  * Change so that it takes protocols as well so that those can be loaded into memory when the users 
  *  are created and added to the universe.
  *)
-Fixpoint generate_universe (cmds_list : list cmd) : universe :=
+Fixpoint generate_universe (cmds_list : list cmd) (user_id_list : list user_id) : universe :=
 let pairs_list := (generate_all_pairs user_id_list) in
   let umap := (add_users user_id_list) in
    let ch_id' := (length user_id_list) * ((length user_id_list) - 1) / 2 in
