a51bf396edc57f6138d0eb5f61f9140c5bc0a9bc
Timothy Braje <tim.braje@gmail.com>
2020-03-17 16:52:55 -0400

    Starting to get implementation of encryption/decryption/keygen implemented

--- haskell/src/Effects/CryptoniteInterpreter.hs
+++ haskell/src/Effects/CryptoniteInterpreter.hs
@@ -26,11 +26,20 @@ module Effects.CryptoniteInterpreter
 import           Unsafe.Coerce
 
 import           Crypto.Cipher.AES (AES256)
-import           Crypto.Random (SystemDRG, withDRG)
+import           Crypto.Cipher.Types (BlockCipher(..), Cipher(..), makeIV, IV(..))
+import           Crypto.Error (CryptoFailable(..), CryptoError(..))
+-- Symmetric Signagures
+import           Crypto.MAC.HMAC (hmac)
+-- Asymmetric Operations
+import           Crypto.PubKey.RSA.PKCS15 (decryptSafer, signSafer, encrypt, verify)
+import           Crypto.PubKey.RSA (PublicKey, PrivateKey, generate)
+--import           Crypto.PubKey.Ed25519 (PublicKey, SecretKey, Signature, generateSecretKey, toPublic, sign, verify)
+import           Crypto.Random (MonadRandom, SystemDRG, withDRG)
 import           Crypto.Random.Types (getRandomBytes)
 
 import           Data.ByteArray (ByteArray)
 import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as C8
 
 import           Data.Function ((&))
 import qualified Data.Map.Strict as M
@@ -41,15 +50,68 @@ import           Polysemy.State (State(..), get, put)
 import           Effects
 import           Interpreter
 
+import qualified Keys as KS
 import           Messages
 import qualified RealWorld as R
 
 
 data CryptoKey =
-    SymmetricKey ByteString
-  | AsymmetricKey ByteString ByteString
+    SymmKey ByteString
+  | AsymKey PublicKey PrivateKey
+  -- | SymmSigKey ByteString 
+  -- | AsymSigKey PublicKey PrivateKey
 
-type StoredCipher = MsgPayload
+data StoredCipher =
+  StoredPermission Bool Key ByteString
+  | StoredContent ByteString
+  | StoredPair StoredCipher StoredCipher
+
+strToBs :: Show a => a -> ByteString
+strToBs = C8.pack . show
+
+mkEncCipher :: MonadRandom m =>
+  M.Map Int CryptoKey -> (ByteString -> m ByteString) -> MsgPayload -> m StoredCipher
+mkEncCipher keys enc (R.Coq_message__Permission (kid,tf)) =
+  let k = keys M.! kid
+  in  StoredPermission tf kid <$>
+       case k of
+         SymmKey bs       -> enc bs
+         AsymKey pub priv -> if tf then (enc . strToBs) priv else (enc . strToBs) pub
+
+mkEncCipher _ enc (R.Coq_message__Content i) =
+  StoredContent <$> (enc . C8.pack . show $ i)
+
+mkEncCipher keys enc (R.Coq_message__MsgPair _ _ m1 m2) =
+  do c1 <- mkEncCipher keys enc m1
+     c2 <- mkEncCipher keys enc m2
+     return $ StoredPair c1 c2
+  
+decryptCipher :: M.Map Int CryptoKey -> (ByteString -> ByteString) -> StoredCipher -> MsgPayload
+decryptCipher keys dec (StoredPermission tf kid bs) =
+  let k = keys M.! kid
+      plain = dec bs
+  in  R.Coq_message__Permission $
+       case k of -- probably want to be handing around keys here, and not ids
+         SymmKey _   -> (kid,True)
+         AsymKey _ _ -> (kid,tf)
+
+decryptCipher _ dec (StoredContent bs) =
+  case C8.readInt (dec bs) of
+    Nothing    -> error "Decrypting content didn't work"
+    Just (m,_) -> R.Coq_message__Content m
+
+decryptCipher keys dec (StoredPair c1 c2) =
+  let m1 = decryptCipher keys dec c1
+      m2 = decryptCipher keys dec c2
+  in  R.Coq_message__MsgPair Nat Nat m1 m2 -- do I need to track types
+
+ --  data Coq_message__Coq_message =
+ --   Coq_message__Permission RW_message__Coq_access
+ -- | Coq_message__Content Prelude.Int
+ -- | Coq_message__MsgPair Messages.Coq_type Messages.Coq_type Coq_message__Coq_message Coq_message__Coq_message
+
+
+
 
 data CryptoData = CryptoData {
     ciphers :: M.Map Int StoredCipher
@@ -58,14 +120,14 @@ data CryptoData = CryptoData {
   }
 
 -- | Add provided cipher in the next slot
-addCipher :: StoredCipher -> CryptoData -> (Int,CryptoData)
-addCipher v cryptoData@CryptoData{..} =
+addCipher :: SystemDRG -> StoredCipher -> CryptoData -> (Int,CryptoData)
+addCipher drg' cipher cryptoData@CryptoData{..} =
   let k =
         case M.lookupMax ciphers of
           Nothing -> 0
           Just (k',_) -> k'
-  in
-    (k, cryptoData { ciphers = M.insert k v ciphers })
+  in 
+    (k, cryptoData { ciphers = M.insert k cipher ciphers, drg = drg' })
 
 -- | Add provided key in the next slot
 addKey :: SystemDRG -> CryptoKey -> CryptoData -> (Int,CryptoData)
@@ -77,34 +139,64 @@ addKey d v cryptoData@CryptoData{..} =
   in
     (k, cryptoData { keys = M.insert k v keys, drg = d })
 
+genRandomIV :: (MonadRandom m, BlockCipher c) => c -> m (IV c)
+genRandomIV c = do
+  bs :: ByteString <- getRandomBytes (blockSize c)
+  return $
+    case (makeIV bs) of
+      Nothing -> error "Couldn't make IV"
+      Just iv -> iv
+
+encryptMessage :: MonadRandom m => CryptoKey -> ByteString -> m ByteString
+encryptMessage (AsymKey pub _) msg = do
+  eitherMsg <- encrypt pub msg
+  return $ case eitherMsg of
+             Left e -> error $ "Error asymmetrically encrypting: " ++ show e
+             Right m -> m
+encryptMessage (SymmKey k) msg = do
+  iv <- genRandomIV (undefined :: AES256)
+  return $ case cipherInit k of
+             CryptoFailed e -> error $ "Symmetric encrypt failed: " ++ show e
+             CryptoPassed c -> ctrCombine c iv msg
+
+decryptMessage :: MonadRandom m => CryptoKey -> ByteString -> m ByteString
+decryptMessage (AsymKey _ priv) msg = do
+  eitherMsg <- decryptSafer priv msg
+  return $ case eitherMsg of
+             Left e -> error $ "Error asymmetrically decrypting: " ++ show e
+             Right m -> m
+decryptMessage key msg =
+  encryptMessage key msg
 
 runCryptoWithCryptonite :: Member (State CryptoData) r
   => Sem (Crypto : r) a -> Sem r a
 runCryptoWithCryptonite = interpret $ \case
   MkSymmetricKey _  -> do
     cryptData <- get
-    -- generate the key
+    -- generate the key (23*8 = 256 bits)
     let (bs, drg') = withDRG (drg cryptData) (getRandomBytes 32)
-    let (k,cryptData') = addKey drg'  (SymmetricKey bs) cryptData
+    let (k,cryptData') = addKey drg'  (SymmKey bs) cryptData
     _ <- put cryptData'
     (return . unsafeCoerce) (k,True)
   MkAsymmetricKey _  -> do
     cryptData <- get
-    -- generate the key INCORRECT/PLACEHOLDER
-    let (bs, drg') = withDRG (drg cryptData) (getRandomBytes 32)
-    let (k,cryptData') = addKey drg' (AsymmetricKey bs bs) cryptData
+    -- generate RSA pub/priv key
+    let ((pub,priv), drg') = withDRG (drg cryptData) (generate 4096 0x10001)
+    let (k,cryptData') = addKey drg' (AsymKey pub priv) cryptData
     _ <- put cryptData'
     (return . unsafeCoerce) (k,True)
   SignMessage _ _ _ m -> do
-    cryptData <- get
-    let (k,cryptData') = addCipher m cryptData
+    cryptData@CryptoData{..} <- get
+    let (cid,cryptData') = addCipher drg undefined cryptData -- FIXME
     _ <- put cryptData'
-    (return . unsafeCoerce) (R.SignedCiphertext Nat k)
-  SignEncryptMessage _ _ _ _ m -> do
-    cryptData <- get
-    let (k,cryptData') = addCipher m cryptData
+    (return . unsafeCoerce) (R.SignedCiphertext Nat cid)
+  SignEncryptMessage _ _ kenc _ m -> do
+    cryptData@CryptoData{..} <- get
+    let k = keys M.! kenc
+    let (cipher,drg')    = withDRG drg (mkEncCipher keys (encryptMessage k) m)
+    let (cid,cryptData') = addCipher drg' cipher cryptData
     _ <- put cryptData'
-    (return . unsafeCoerce) (R.SignedCiphertext Nat k)
+    (return . unsafeCoerce) (R.SignedCiphertext Nat cid)
   VerifyMessage _ _ (R.SignedCiphertext _ k) -> do
     cryptData <- get
     let m = (ciphers cryptData) M.! k -- use (!) or explicitly case on Maybe?
