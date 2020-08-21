0472e7109557026dfb2df9cbd8cfecdfef85c49a
Timothy Braje <tim.braje@gmail.com>
2020-08-21 09:56:52 -0400

    Remove sets from prelude

--- KeyManagement/MyPrelude.v
+++ KeyManagement/MyPrelude.v
@@ -18,7 +18,7 @@
 From Coq
      Require Import Eqdep String Arith Lia Program Bool.
 
-Require Import StepRelations Sets.
+Require Import StepRelations.
 
 (* From Frap *)
 (*      Require Import *)
@@ -200,30 +200,5 @@ Lemma eq_iff : forall P Q,
 Proof.
   equality.
 Qed.
-
-Ltac sets0 := Sets.sets ltac:(simpl in *; intuition (subst; auto; try equality; try linear_arithmetic)).
-
-Ltac sets := propositional;
-  try match goal with
-      | [ |- @eq (?T -> Prop) _ _ ] =>
-        change (T -> Prop) with (set T)
-      end;
-  try match goal with
-      | [ |- @eq (set _) _ _ ] =>
-        let x := fresh "x" in
-        apply sets_equal; intro x;
-        repeat match goal with
-               | [ H : @eq (set _) _ _ |- _ ] => apply (f_equal (fun f => f x)) in H;
-                                                apply eq_iff in H
-               end
-      end; sets0;
-  try match goal with
-      | [ H : @eq (set ?T) _ _, x : ?T |- _ ] =>
-        repeat match goal with
-               | [ H : @eq (set T) _ _ |- _ ] => apply (f_equal (fun f => f x)) in H;
-                                                 apply eq_iff in H
-               end;
-          solve [ sets0 ]
-      end.
 
 Remove Hints absurd_eq_true trans_eq_bool.
